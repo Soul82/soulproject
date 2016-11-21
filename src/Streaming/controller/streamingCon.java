c3676e59441c44e1ae97cdf15cc9fc2346cae6d6
chungil <dlcnddlf88@naver.com>
2016-11-22 00:55:48 +0900

    ## aws file upload해서 DB에 쌓기 DB 필수 필요값 파일이름 + 경로+파일이름 + 클릭 수(number) uuid+ 파일크기

--- src/Streaming/controller/streamingCon.java
+++ src/Streaming/controller/streamingCon.java
@@ -7,13 +7,15 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.audio.mp3.MP3File;
-import org.jaudiotagger.tag.FieldKey;
-import org.jaudiotagger.tag.Tag;
-import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Controller
 public class streamingCon {
@@ -24,58 +26,40 @@ public class streamingCon {
 		return "body:streaming/1";
 	}
 
+	
+	@RequestMapping("/mp3up")
+	public String upload(){
+		return "/soulplayer/mp3Upload";
+	}
+	
 	@RequestMapping("/music/play")
 	public void soundPlay() {
 		URLConnection conn = null;
-		try{
-		conn = new URL("https://soundcloud.com/1990han/sm-station-x-exo-dancing-king-cover").openConnection(); // fill in the correct http
-												// address
-		InputStream is = conn.getInputStream();
+		try {
+			conn = new URL("https://soundcloud.com/1990han/sm-station-x-exo-dancing-king-cover").openConnection(); // fill
+																													// in
+																													// the
+																													// correct
+																													// http
+			// address
+			InputStream is = conn.getInputStream();
 
-		OutputStream outstream = new FileOutputStream(new File("c:/users/admin/test.mp3")); // fill
-																							// in
-		byte[] buffer = new byte[4096];
-		int len;
+			OutputStream outstream = new FileOutputStream(new File("c:/users/admin/test.mp3")); // fill
+																								// in
+			byte[] buffer = new byte[4096];
+			int len;
 
-		while ((len = is.read(buffer)) > 0) {
-			outstream.write(buffer, 0, len);
-			System.out.println(len);
-		}
-		outstream.close(); // in a finally block of course
-		is.close();
-		System.out.println("DONE!");
-		
-		
-		}catch(Exception e){
+			while ((len = is.read(buffer)) > 0) {
+				outstream.write(buffer, 0, len);
+				System.out.println(len);
+			}
+			outstream.close(); // in a finally block of course
+			is.close();
+			System.out.println("DONE!");
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	@RequestMapping("/tagger")
-	public void tagger(){
-			String decoding = "ISO-8859-1";
-	        String encoding = "EUC-KR";
-	        File file = new File("ParkHyoShin-Breath.mp3");
-	        try{
-	        MP3File mp3 = (MP3File) AudioFileIO.read(file);
-	//  ID3v24Tag tag24 = mp3.getID3v2TagAsv24();
-	        AbstractID3v2Tag tag2 = mp3.getID3v2Tag();
-	 
-	        Tag tag = mp3.getTag();
-	        String title = tag.getFirst(FieldKey.TITLE);
-	        String artist = tag.getFirst(FieldKey.ARTIST);
-	        String album = tag.getFirst(FieldKey.ALBUM);
-	        String year = tag.getFirst(FieldKey.YEAR);
-	        String genre = tag.getFirst(FieldKey.GENRE);
-	 
-	        System.out.println("Tag : " + tag2);
-	        System.out.println("Song Name : " + title);
-	        System.out.println("Artist : " + artist);
-	        System.out.println("Album : " + album);
-	        System.out.println("Year : " + year);
-	        System.out.println("Genre : " + genre);
-        }catch(Exception e){
-        	e.printStackTrace();
-        }
-	}
+
 }
